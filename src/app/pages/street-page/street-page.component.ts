b53a3d8050e5ea5ce82c34b9418a61453bb153a6
cesarb03 <cesar_b_03@outlook.com>
2022-12-27 22:21:27 -0300

    clase10-firstcommit

---
+++ src/app/pages/street-page/street-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-street-page',
+  templateUrl: './street-page.component.html',
+  styleUrls: ['./street-page.component.scss']
+})
+export class StreetPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
